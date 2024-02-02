f025afc8b99d3a2532f94abb4c9a80800c243bfa
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-02-02 20:05:05 +0100

    backport of 75640308d436d3648e3161d7e0b185d653c1b95e

--- extension.js
+++ extension.js
@@ -144,6 +144,11 @@ class Extension {
 
         if (!this._panel) return;
 
+        if (this._sink_mixer) {
+            this._sink_mixer.destroy();
+            this._sink_mixer = null;
+        }
+
         if (this._applications_mixer) {
             this._applications_mixer.destroy();
             this._applications_mixer = null;

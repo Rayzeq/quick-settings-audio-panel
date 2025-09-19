4716c41b1688c9951bacdae20cbfef9e1bc2e4e6
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2025-09-19 23:27:43 +0200

    fix: prevent harmless error at build time

--- copy-libpanel.sh
+++ copy-libpanel.sh
@@ -5,5 +5,7 @@ LIBPANEL_DST=./dist/libs/libpanel
 
 mkdir -p $(dirname $LIBPANEL_DST)
 
-rm -r $LIBPANEL_DST
+if [ -d $LIBPANEL_DST ]; then
+	rm -r $LIBPANEL_DST
+fi
 cp -r $LIBPANEL_SRC $LIBPANEL_DST
